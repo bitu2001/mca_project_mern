64c7eb06872d7345bc9df35987ce9bce88173ab9
Bitu <tripathysuvendu5@gmail.com>
2024-02-01 11:57:15 +0530

    kbhk

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import DrAppointment from './Pages/DrAppointment'
+import ForgotPassword from './Pages/ForgotPassword';
+import Signup from './Pages/Signup';
+import ResetPassword from "./Pages/ResetPassword";
+import Login from "./Pages/Login";
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App
