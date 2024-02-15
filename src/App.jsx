db74f7268b65982f7f136d47af0eb198f519add8
Bitu <tripathysuvendu5@gmail.com>
2024-02-15 20:18:03 +0530

    hided navbar and footer after clicking on ligin signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import Navbar from "./Components/Navbar";
-import { Routes, Route } from "react-router-dom";
-import Footer from "./Pages/Footer";
-import Home from "./Components/Home";
-import Notification from "./Components/Notification";
-import Login from './Pages/Login'
-import Signup from "./Pages/Signup";
+
+import Layout from "./Layout/Layout";
+
 function App() {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
-      console.log('lat is'+lat)
-      console.log('long is'+long)
-    });
-  } else {
-  }
 
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/notification" element={<Notification />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
-      <Footer />
+      <div>
+        <Layout/>
+        
+      </div>
     </>
   );
 }
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "../Components/Navbar";
+import Home from "../Components/Home";
+import Notification from "../Components/Notification";
+import Footer from "../Pages/Footer";
+import Login from "../Pages/Login";
+import Signup from "../Pages/Signup";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function Layout() {
+  const location = useLocation();
+  const [hideNavbar, setHideNavbar] = useState(false);
+  const [hideFooter, setHideFooter] = useState(false);
+
+  React.useEffect(() => {
+    if (location.pathname === "/login" || location.pathname === "/signup") {
+      setHideNavbar(true);
+      setHideFooter(true);
+    } else {
+      setHideNavbar(false);
+      setHideFooter(false);
+    }
+  }, [location]);
+
   return (
     <div>
-      
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      {!hideFooter && <Footer />}
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
